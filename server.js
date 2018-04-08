ba956c0e3565a68a23e50f3353898943fbb3a5a2
Sunjoong Kim <sunjoong85@gmail.com>
2018-04-08 15:23:33 +0900

    test

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ const options = {
 }
 
 //http.createServer(app.callback()).listen(3001);
+const h1server = http.createServer().listen(3001);
 
 const h2server = http2.createSecureServer(options);
-
-h2server.listen(3000);
+//h2server.listen(3000);
 
 // The files are pushed to stream here
 function push(stream, reqPath) {
